2ad38d2bc2ac810b8ffbebe29b425cf866f13da6
Shyhiem Gilbert <shyhiemg@icloud.com>
2018-12-07 02:29:57 +0000

    Tried to fix Total page error

---
+++ Mvc Calculator/Controllers/Control_calcController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Mvc_Calculator.Controllers
+{
+	public class Control_calcController : Controller
+	{
+		// GET: Control_calc
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public ActionResult Total()
+		{
+			return View();
+		}
+
+		//For when calculate button is pressed
+		[HttpPost]
+		public ActionResult Total(int value1, int value2, String calci)
+		{
+			int Total = 0;
+			switch(calci)
+			{
+				case "+":
+					Total = value1 + value2;
+					break;
+				case "-":
+					Total = value1 - value2;
+					break;
+				case "*":
+					Total = value1 * value2;
+					break;
+				case "/":
+					Total = value1 / value2;
+					break;
+				//case "^":
+				//	Total = (int)Math.Pow(value1,value2);
+				//	break;
+				//case "%":
+				//	Total = value1 % value2;
+				//	break;
+				default:
+					break;
+			}
+			return Content("The Answer Is" + " "+ Total);
+
+		}
+	}
+}
--- Mvc Calculator/Global.asax.cs
+++ Mvc Calculator/Global.asax.cs
@@ -16,6 +16,7 @@ namespace Mvc_Calculator
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			GlobalFilters.Filters.Add(new HandleErrorAttribute());
 		}
 	}
 }
